1a346275c56bae2ff3ee0fb46fb1afd973d58e57
Lena Eivy <41176683+applena@users.noreply.github.com>
2019-01-15 20:52:07 -0800

    Revert "took notes in this branch and fixed the typo"

--- src/middleware/model-finder.js
+++ src/middleware/model-finder.js
@@ -2,7 +2,6 @@
 
 module.exports = (req, res, next) => {
   let modelName = req.params.model;
-  // console.log(`loading ../models/${modelName}.js`);
-  req.model = require(`../models/${modelName}Model.js`);
+  req.model = require(`../models/${modelName}.js`);
   next();
 };
--- src/models/productsSchema.js
+++ src/models/productsSchema.js
@@ -4,11 +4,8 @@ const mongoose = require('mongoose');
 
 const products = mongoose.Schema({
   name: {type: String, required:true },
-  display_name: { type: String, required: false},
+  display_name: { type: String, required: true},
   description: { type: String, required: false },
-
-  category: { type: String, required: false, enum:['book', 'person', 'computer']},
-
   updatedAt: { type: Date, required: false },
   category: { type: String, required: false, enum:['book', 'person', 'computer']},
 });
