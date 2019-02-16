8764a55133c74816b9cb72567847809924d495b0
LENA EIVY <applena@gmail.com>
2019-02-15 19:54:58 -0800

    console logs added

--- src/lib/publisher.js
+++ src/lib/publisher.js
@@ -5,7 +5,14 @@ const SERVER = process.env.Q_SERVER || 'http://localhost:3333';
 class Q {
 
   constructor(q) {
+    console.log(`connecting to socket io server ${SERVER}`);
     this.q = io.connect(`${SERVER}`);
+    io.on('event', function(data){
+      console.log('io event', data);
+    });
+    io.on('disconnect', function(){
+      console.log('io disconnect');
+    });
   }
 
   /**
